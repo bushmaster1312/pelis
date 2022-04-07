c45a748e7d53590b23ce750b3eca7b28239f382a
bushmaster1312 <marianodominguez1992@gmail.com>
2022-04-07 20:42:23 -0300

    segundo

--- src/components/Navbar/Nabvar.js
+++ src/components/Navbar/Nabvar.js
@@ -16,7 +16,7 @@ const Nabvar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-         <Link class="nav-link active" aria-current="page" to={`/`}>Inicio</Link>
+         <Link class="nav-link active" aria-current="page" to={`/`}>Ilnicio</Link>
         </li>
        
         <li className="nav-item dropdown">
